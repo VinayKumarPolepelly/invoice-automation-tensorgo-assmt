9afac568d414110802ebc2871cb69270c15faca6
Nikhildyag <157957987+Nikhildyag@users.noreply.github.com>
2024-04-17 11:23:43 +0530

    Update LeaveReport.js

--- Frontend/src/components/Employee/LeaveReport.js
+++ Frontend/src/components/Employee/LeaveReport.js
@@ -112,18 +112,4 @@ const LeaveReport = () => {
   );
 };
 
-export default LeaveReport;
-import React from 'react'
-import EmployeeHeader from './EmployeeHeader'
-
-const LeaveReport = () => {
-  return (
-    <div>
-      <div>
-        <EmployeeHeader />
-      </div>
-    </div>
-  )
-}
-
 export default LeaveReport
