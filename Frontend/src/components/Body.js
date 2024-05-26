5307808d5cd8e6d83c3f624d07635d288ff7e6b0
Nikhil Reddy <nikhildyaga123@gmail.com>
2024-05-26 18:28:23 +0530

    added the map feature

--- Frontend/src/components/Body.js
+++ Frontend/src/components/Body.js
@@ -10,6 +10,18 @@ const Body = () => {
           alt="this is homepage log"
         />
       </div>
+      <div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3775.1244965248547!2d77.91919617466056!3d18.881557358049122!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcde18c82d2cdb1%3A0x469505d0e802dd0c!2sBasara%20IIIT%20Campus!5e0!3m2!1sen!2sin!4v1716727977895!5m2!1sen!2sin"
+          width="600"
+          height="450"
+          style={{ border: 0 }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+          className="ml-96"
+        ></iframe>
+      </div>
     </div>
   );
 };
