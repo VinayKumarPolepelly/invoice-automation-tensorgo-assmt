a74e07e255db3203ba8fb622cc8a92dcad75d38e
Nikhil Reddy <nikhildyaga123@gmail.com>
2024-05-30 11:56:09 +0530

    website

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ import adminRouter from "./routes/admin.routes.js";
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000", // Frontend domain
+  origin: "https://hr-3.onrender.com/", // Frontend domain
   credentials: true, // Allow credentials (cookies) to be sent
 };
 
